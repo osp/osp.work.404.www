2be3a93275d66555e27fffc76e73dfd05c1911a7
Pierre Marchand <pierre@oep-h.com>
2013-01-28 12:23:43 +0100

    Added applications

--- fzf/fzf/settings.py
+++ fzf/fzf/settings.py
@@ -102,10 +102,12 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'zinnia',
+    'south',
+    'fzf'
 )
 
 # A sample logging configuration. The only tangible logging
