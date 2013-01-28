9e12492c55f4e95cbb05bfe5ae7419842b90511c
Alexandre Leray <alexandre@alexandreleray.com>
2013-01-28 13:23:56 +0100

    Fixed the url pattern for Zinna's app inclusion

--- fzf/fzf/urls.py
+++ fzf/fzf/urls.py
@@ -7,5 +7,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^comments/', include('django.contrib.comments.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include('zinnia.urls')),
+    url(r'^', include('zinnia.urls')),
 )
