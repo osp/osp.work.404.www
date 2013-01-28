715d47f705aedf35446b98b53355369d38d88812
Pierre Marchand <pierre@oep-h.com>
2013-01-28 12:47:46 +0100

    Adjusted settings and urls

--- fzf/fzf/urls.py
+++ fzf/fzf/urls.py
@@ -5,5 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^comments/', include('django.contrib.comments.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', include('zinnia.urls')),
 )
